63113412aacd6aca4b2b57fa0e837817fd3d1967
h248 <go0og@walla.com>
2016-01-02 15:51:42 +0200

    Empty C# file
    Added an empty C# file to make GitHub's liguist mark this repo with the
    C# tag

---
+++ dummy.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace h248Parser
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+	// Only reason this file exists is to achieve the C# tag when searching GitHub.
+	// Refer to the README.md file for the parser creation guide.
+	}
+    }
+}
